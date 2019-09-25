312412d182d36057d4f7f5e2c383c2d7f3b47d36
mdhar6 <mdhar6@c02yl2jqjgh6.uk.aexp.com>
2019-09-25 09:06:19 +0100

    Latest commit

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -18,19 +18,28 @@ public class Pawn extends AbstractPiece {
 
         ArrayList<Move> movesArr = new ArrayList<>();
 
-        int direction = getColour().equals(PlayerColour.WHITE) ?
-                -1 : 1;
+        int direction = getColour().equals(PlayerColour.WHITE) ? -1 : 1;
 
-        Coordinates to = new Coordinates(from.getRow() + direction, from.getCol());
+        Coordinates to1square = new Coordinates(from.getRow() + direction, from.getCol());
+        Coordinates to1squareDiagonalLeft = new Coordinates(from.getRow() + direction, from.getCol() - 1);
+        Coordinates to1squareDiagonalRight = new Coordinates(from.getRow() + direction, from.getCol() + 1);
+
         if(isPawnStartRow(from)) {
-            Coordinates to2 = new Coordinates(from.getRow() + 2 * direction, from.getCol());
-            Move move2 = new Move(from, to2);
+            Coordinates to2squares = new Coordinates(from.getRow() + 2 * direction, from.getCol());
+            Move move2 = new Move(from, to2squares);
             movesArr.add(move2);
         }
 
-        if(!isPieceInFront( board, direction, from )) {
-            Move move = new Move(from, to);
-            movesArr.add(move);
+        if(!isPawnEndRow( from )) {
+            if(!isPawnInFront(board, direction, from)) {
+                Move moveDiagonalLeft = new Move(from, to1squareDiagonalLeft);
+                Move moveDiagonalRight = new Move(from, to1squareDiagonalRight);
+                movesArr.add(moveDiagonalLeft);
+                movesArr.add(moveDiagonalRight);
+                Move move = new Move(from, to1square);
+                movesArr.add(move);
+            }
+
         }
 
         return movesArr;
@@ -42,10 +51,20 @@ public class Pawn extends AbstractPiece {
         return getColour().equals(PlayerColour.WHITE) && from.getRow() == 6 || getColour().equals(PlayerColour.BLACK) && from.getRow() == 1;
     }
 
-
-    private boolean isPieceInFront( Board board, int direction, Coordinates from ) {
+    private boolean isPawnInFront( Board board, int direction, Coordinates from ) {
         Coordinates squareInFront = new Coordinates( from.getRow() + direction, from.getCol() );
         return board.get(squareInFront) != null;
     }
+
+    private boolean isPawnEndRow( Coordinates from ) {
+        return getColour().equals(PlayerColour.WHITE) && from.getRow() == 0 || getColour().equals(PlayerColour.BLACK) && from.getRow() == 7;
+    }
+//
+//    private boolean isPawnDiagonal( Board board, int direction, Coordinates from ) {
+//        // if pawn is NOT in a diagonal square
+//            // do not allow current pawn to move
+//        // only allow to move if pawn can capture
+//
+//    };
 
 }
